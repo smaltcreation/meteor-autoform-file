b6680319272a6c9044218a19aba8afbc71e4c558
Wassim Dhif <wassimdhif@gmail.com>
2015-10-13 16:25:36 +0200

    Ajout des sources

---
+++ package.js
@@ -0,0 +1,34 @@
+Package.describe({
+    name: 'smalt:autoform-file',
+    version: '0.0.1',
+    summary: 'File input for autoform'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.1.0.2');
+    api.export('Smalt', 'client');
+
+    // Packages
+    api.use([
+        'underscore',
+        'templating',
+        'tracker',
+        'aldeed:autoform@5.3.2'
+    ], 'client');
+
+    // Files
+    api.addFiles([
+        // Lib
+        'lib/client/lib/container.js',
+        'lib/client/lib/file/new.js',
+        'lib/client/lib/file/uploaded.js',
+        'lib/client/lib/input.js',
+        // Input
+        'lib/client/input/input.html',
+        'lib/client/input/input.js',
+        // Preview
+        'lib/client/preview/preview.html',
+        'lib/client/preview/preview.js',
+        'lib/client/preview/preview.css'
+    ], 'client');
+});
